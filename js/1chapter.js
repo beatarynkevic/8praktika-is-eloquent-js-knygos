1300382729a9d4e95120df0eed5d019c31a958d3
beatarynkevic <beatainn@gmail.com>
2020-11-16 11:42:57 +0200

    chapter

---
+++ js/1chapter.js
@@ -0,0 +1,21 @@
+const first = 'Vol';
+const second = 'ca';
+const third = 'no';
+
+const word = first + second +third;
+console.log(word);
+
+console.log(`half of 100 is ${100/2}`);
+
+//typeof operator
+console.log(typeof 4,5);
+console.log(typeof 'x');
+console.log(typeof true);
+console.log(typeof [1]);
+
+//boolean value
+console.log(3<2);
+
+//&& logical operator and (jeigu abu true)
+//|| or (jeigu bent vienas true)
+//! not
